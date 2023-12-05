24624f96dde676151dac02e2448f9d20726c15f7
jacques <jac.granarolo.info@gmail.com>
2023-12-05 17:05:46 +0100

    passage du table html en IonGrid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,32 @@
-import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
 //import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
-import  { getTasks, updateTask, addTask, deleteTask }  from '../services/JsonServerService';
-import { useEffect, useState } from 'react';
-import { bufferCount, fromEvent } from 'rxjs';
+import "./Home.css";
+import {
+  getTasks,
+  updateTask,
+  addTask,
+  deleteTask,
+} from "../services/JsonServerService";
+import { useEffect, useState } from "react";
+import { bufferCount, fromEvent } from "rxjs";
 
 const Home: React.FC = () => {
   const [tasks, setTasks] = useState<ITask[]>([]);
   const [newTask, setNewTask] = useState<string>("");
+
   useEffect(() => {
     const tasks$ = getTasks();
     const subscription = tasks$.subscribe((data) => {
@@ -17,22 +36,24 @@ const Home: React.FC = () => {
         }
         return Number(a.completed) - Number(b.completed);
       });
-      setTasks(data)});
+      setTasks(data);
+    });
     return () => subscription.unsubscribe(); // clean up on component unmount
   }, []);
 
   useEffect(() => {
-    const addButton = document.querySelector('ion-button') as HTMLIonButtonElement;
-    const click$ = fromEvent(addButton, 'click');
+    const addButton = document.querySelector(
+      "ion-button"
+    ) as HTMLIonButtonElement;
+    const click$ = fromEvent(addButton, "click");
     const doubleClick$ = click$.pipe(bufferCount(2));
-   
+
     const subscription = doubleClick$.subscribe(() => {
       handleAddTask();
     });
-   
+
     return () => subscription.unsubscribe();
-   }, [handleAddTask]);
-   
+  }, [handleAddTask]);
 
   /*
   const toggleCompletion = (taskId: number) => {
@@ -42,39 +63,43 @@ const Home: React.FC = () => {
   };
 */
 
-const toggleCompletion = (taskId: number) => {
-  const task = tasks.find(task => task.id === taskId);
-  if (task) {
-    const updatedTask = {...task, completed: !task.completed};
-    const update$ = updateTask(taskId, updatedTask.completed);
-    update$.subscribe(() => {
-      setTasks(tasks.map(task => 
-        task.id === taskId ? updatedTask : task
-      ).sort((a: ITask, b: ITask) => {
-        if (a.completed === b.completed) {
-          return a.id - b.id;
-        }
-        return Number(a.completed) - Number(b.completed);
-      }));
+  const toggleCompletion = (taskId: number) => {
+    const task = tasks.find((task) => task.id === taskId);
+    if (task) {
+      const updatedTask = { ...task, completed: !task.completed };
+      const update$ = updateTask(taskId, updatedTask.completed);
+      update$.subscribe(() => {
+        setTasks(
+          tasks
+            .map((task) => (task.id === taskId ? updatedTask : task))
+            .sort((a: ITask, b: ITask) => {
+              if (a.completed === b.completed) {
+                return a.id - b.id;
+              }
+              return Number(a.completed) - Number(b.completed);
+            })
+        );
+      });
+    }
+  };
+
+  const deleteTaskById = (taskId: number) => {
+    const delete$ = deleteTask(taskId);
+    delete$.subscribe(() => {
+      setTasks(
+        tasks
+          .filter((task) => task.id !== taskId)
+          .sort((a: ITask, b: ITask) => {
+            if (a.completed === b.completed) {
+              return a.id - b.id;
+            }
+            return Number(a.completed) - Number(b.completed);
+          })
+      );
     });
-  }
-};
+  };
 
-const deleteTaskById = (taskId: number) => {
-  const delete$ = deleteTask(taskId);
-  delete$.subscribe(() => {
-    setTasks(tasks.filter(task => task.id !== taskId)
-    .sort((a: ITask, b: ITask) => {
-      if (a.completed === b.completed) {
-        return a.id - b.id;
-      }
-      return Number(a.completed) - Number(b.completed);
-    }));
-  });
-};
-
-
-/*
+  /*
 
 const handleAddTask = () => {
   const task = { title: newTask, completed: false };
@@ -92,29 +117,27 @@ const handleAddTask = () => {
   });
 };*/
 
-function handleAddTask () {
-  const task = { title: newTask, completed: false };
-  const add$ = addTask(task);
-  add$.subscribe((addedTask) => {
-    const newTasks = [...tasks, addedTask];
-    newTasks.sort((a: ITask, b: ITask) => {
-      if (a.completed === b.completed) {
-        return a.id - b.id;
-      }
-      return Number(a.completed) - Number(b.completed);
+  function handleAddTask() {
+    const task = { title: newTask, completed: false };
+    const add$ = addTask(task);
+    add$.subscribe((addedTask) => {
+      const newTasks = [...tasks, addedTask];
+      newTasks.sort((a: ITask, b: ITask) => {
+        if (a.completed === b.completed) {
+          return a.id - b.id;
+        }
+        return Number(a.completed) - Number(b.completed);
+      });
+      setTasks(newTasks);
+      setNewTask("");
     });
-    setTasks(newTasks);
-    setNewTask("");
-  });
- };
-
- 
+  }
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle className='text-center'>Gestion des tâches</IonTitle>
+          <IonTitle className="text-center">Gestion des tâches</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -123,36 +146,49 @@ function handleAddTask () {
             <IonTitle size="large">Gestion des tâches</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonItem className='form-new-task'>
-          <IonLabel >Nouvelle tâche :</IonLabel>
-          <IonInput value={newTask} onIonChange={(event) => setNewTask(event.detail.value!)} />
-        <IonButton color='primary' onClick={handleAddTask}>Ajouter</IonButton>
+        <IonItem className="form-new-task">
+          <IonLabel>Nouvelle tâche :</IonLabel>
+          <IonInput
+            value={newTask}
+            onIonChange={(event) => setNewTask(event.detail.value!)}
+          />
+          <IonButton color="primary" onClick={handleAddTask}>
+            Ajouter
+          </IonButton>
         </IonItem>
-        <div className='div-tasks'>
-        <table className='table-tasks'>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Titre</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tasks.map(task => (
-              <tr key={task.id}>
-                <td>{task.id}</td>
-                <td className={task.completed ? 'checked' : ''} onDoubleClick={() => toggleCompletion(task.id)}>{task.title}</td>
-                <td>
-                <IonButton color="success" onClick={() => toggleCompletion(task.id)}>
-                    {task.completed ? 'Invalider' : 'Valider'}
+        <div className="div-tasks">
+          <IonGrid className="table-tasks">
+            <IonRow>
+              <IonCol>ID</IonCol>
+              <IonCol>Titre</IonCol>
+              <IonCol>Actions</IonCol>
+            </IonRow>
+            {tasks.map((task) => (
+              <IonRow key={task.id}>
+                <IonCol>{task.id}</IonCol>
+                <IonCol
+                  className={task.completed ? "checked" : ""}
+                  onDoubleClick={() => toggleCompletion(task.id)}
+                >
+                  {task.title}
+                </IonCol>
+                <IonCol>
+                  <IonButton
+                    color={task.completed ? "warning" : "success"}
+                    onClick={() => toggleCompletion(task.id)}
+                  >
+                    {task.completed ? "Invalider" : "Valider"}
                   </IonButton>
-
-                  <IonButton color="danger" onClick={() => deleteTaskById(task.id)}>Supprimer</IonButton>
-                </td>
-              </tr>
+                  <IonButton
+                    color="danger"
+                    onClick={() => deleteTaskById(task.id)}
+                  >
+                    Supprimer
+                  </IonButton>
+                </IonCol>
+              </IonRow>
             ))}
-          </tbody>
-        </table>
+          </IonGrid>
         </div>
       </IonContent>
     </IonPage>
