36acb966f4f477245753005c01e5f661e6ed598a
jacques <jac.granarolo.info@gmail.com>
2023-12-05 12:15:04 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tp_Ionic_Rxjs',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
